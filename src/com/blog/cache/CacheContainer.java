5a9506622c6ddda7c717c570b2b256c654297a5d
张雷 <zl07736@ly.com>
2014-06-20 12:35:33 +0800

    ddd

--- src/com/blog/cache/CacheContainer.java
+++ src/com/blog/cache/CacheContainer.java
@@ -1,5 +1,6 @@
 package com.blog.cache;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 
@@ -8,15 +9,17 @@ import net.sf.ehcache.CacheManager;
 
 @SuppressWarnings("serial")
 public class CacheContainer extends HttpServlet {
-	private static CacheManager cm = CacheManager.create("src/ehcache.xml");
-	private Cache cache = cm.getCache("classes");
-	static{
-		
-	}
+	private static CacheManager cm = null;
 	public void destroy() {
 		
 	}
-	public void init() throws ServletException {
-		
+	
+	public void init(ServletConfig config) throws ServletException {
+		String cacheConfig = config.getInitParameter("cache");
+		CacheManager.create(cacheConfig);
+	}
+	public static Cache getCache(String cache)
+	{
+		return cm.getCache(cache);
 	}
 }
---
+++ src/com/blog/cache/CacheType.java
@@ -0,0 +1,5 @@
+package com.blog.cache;
+
+public class CacheType {
+	private 
+}
